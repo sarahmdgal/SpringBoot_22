2e9a0db9059fc2db27dee85f943da614ce3bf8da
sarahmdgal <sarahrubin2@yahoo.com>
2017-08-25 08:55:47 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Arrays;
+
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("Loading data . . .");
+
+        roleRepository.save(new Role("USER"));
+        roleRepository.save(new Role("ADMIN"));
+
+        Role adminRole = roleRepository.findByRole("ADMIN");
+        Role userRole = roleRepository.findByRole("USER");
+
+        User user = new
+                User("bob@bob.com", "bob", "Bob", "Bobberson", true, "bob");
+        user.setRoles(Arrays.asList(userRole));
+        userRepository.save(user);
+
+        user  = new
+                User("jim@jim.com", "jim", "Jim", "Jimmerson", true, "jim");
+        user.setRoles(Arrays.asList(userRole));
+        userRepository.save(user);
+
+        user  = new
+                User("admin@adm.com", "pass", "Admin", "User", true, "admin");
+        user.setRoles(Arrays.asList(adminRole));
+        userRepository.save(user);
+
+        user  = new
+                User("sam@ev.com", "pass", "Sam", "Everyman", true, "sam");
+        user.setRoles(Arrays.asList(userRole, adminRole));
+        userRepository.save(user);
+
+    }
+}
